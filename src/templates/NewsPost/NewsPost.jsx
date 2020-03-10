887ab779946cc4345f730ddb71c42ffd70354488
Harley Salas <harley@harleysalas.com>
2020-03-10 07:21:50 +0300

    refined news post template

--- src/templates/NewsPost/NewsPost.jsx
+++ src/templates/NewsPost/NewsPost.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
+import moment from "moment";
 import "./news-post.scss";
 
 import Layout from "../../components/Layout/Layout";
 import ContactCTA from "../../components/ContactCTA/ContactCTA";
+import ArrowLink from "../../components/ArrowLink/ArrowLink";
 
 const BlogPost = ({ data }) => {
   useEffect(() => {
@@ -12,29 +14,101 @@ const BlogPost = ({ data }) => {
   }, []);
   return (
     <Layout>
-      <header className="container news-post__header">
+      <header className="news-post__header">
         <Img
-          fluid={
-            data.markdownRemark.frontmatter.thumbnail.childImageSharp.fluid
-          }
+          fluid={data.current.frontmatter.thumbnail.childImageSharp.fluid}
           fadeIn={true}
           durationFadeIn={1000}
           draggable={false}
-          alt={data.markdownRemark.frontmatter.title}
+          alt={data.current.frontmatter.title}
           className="news-post__header__img-wrapper"
           imgStyle={{
             userSelect: "none",
             opacity: 1,
           }}
         />
-        <div className="news-post__header__img-overlay">
-          <h1 className="news-post__heading">
-            {data.markdownRemark.frontmatter.title}
-          </h1>
-        </div>
+        <div className="news-post__header__img-overlay"></div>
       </header>
-      <div className="container news-post__content-container">
-        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      <div className="news-post__foreground">
+        <div className="container news-post__content-container">
+          <div className="news-post__info-wrapper">
+            <span className="news-post__info">
+              {data.current.frontmatter.category}
+            </span>
+            <span className="news-post__info">
+              {moment(data.current.frontmatter.date).format("MMM Do, YYYY")}
+            </span>
+          </div>
+          <h1 className="news-post__heading">
+            {data.current.frontmatter.title}
+          </h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data.current.html,
+            }}
+          />
+        </div>
+      </div>
+      <div className="container news-post__next-container">
+        {data.next && (
+          <div className="container news-post__next">
+            <div className="news-post__next__content-wrapper news-post__next__content-wrapper--next">
+              <span className="news-post__next__category">
+                {data.next.frontmatter.category}
+              </span>
+              <h2 className="news-post__next__title">
+                {data.next.frontmatter.title}
+              </h2>
+              <ArrowLink
+                linkText="Next Post"
+                linkTo={data.next.fields.slug}
+                linkStyle="black"
+              />
+            </div>
+            <Img
+              fluid={data.next.frontmatter.thumbnail.childImageSharp.fluid}
+              fadeIn={true}
+              durationFadeIn={1000}
+              draggable={false}
+              alt={data.next.frontmatter.title}
+              className="news-post__next__img-wrapper"
+              imgStyle={{
+                userSelect: "none",
+                opacity: 1,
+              }}
+            />
+          </div>
+        )}
+        {data.prev && (
+          <div className="container news-post__next">
+            <Img
+              fluid={data.prev.frontmatter.thumbnail.childImageSharp.fluid}
+              fadeIn={true}
+              durationFadeIn={1000}
+              draggable={false}
+              alt={data.prev.frontmatter.title}
+              className="news-post__next__img-wrapper"
+              imgStyle={{
+                userSelect: "none",
+                opacity: 1,
+              }}
+            />
+            <div className="news-post__next__content-wrapper news-post__next__content-wrapper--prev">
+              <span className="news-post__next__category">
+                {data.prev.frontmatter.category}
+              </span>
+              <h2 className="news-post__next__title">
+                {data.prev.frontmatter.title}
+              </h2>
+              <ArrowLink
+                linkText="Previous Post"
+                linkTo={data.prev.fields.slug}
+                linkStyle="black"
+                backLink
+              />
+            </div>
+          </div>
+        )}
       </div>
       <ContactCTA />
     </Layout>
@@ -42,11 +116,13 @@ const BlogPost = ({ data }) => {
 };
 
 export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
+  query($slug: String!, $prev: String, $next: String) {
+    current: markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
+        category
+        date
         thumbnail {
           childImageSharp {
             fluid(quality: 100) {
@@ -55,6 +131,42 @@ export const query = graphql`
           }
         }
       }
+    }
+    prev: markdownRemark(fields: { slug: { eq: $prev } }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        category
+        date
+        thumbnail {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      excerpt
+    }
+    next: markdownRemark(fields: { slug: { eq: $next } }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        category
+        date
+        thumbnail {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      excerpt
     }
   }
 `;
