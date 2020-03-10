db52ce69d4d73fb8bd966d9dce09b5fabd7f1f2c
Harley Salas <harley@harleysalas.com>
2020-03-10 12:19:48 +0300

    added projects

---
+++ src/pages/projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../components/Layout/Layout";
+import ContactCTA from "../components/ContactCTA/ContactCTA";
+
+const projects = () => {
+  return (
+    <Layout>
+      <h1>Projects</h1>
+      <ContactCTA />
+    </Layout>
+  );
+};
+
+export default projects;
