a2b2dab1ac1843f34aa809d2b9eb2f54d3795fc4
Harley Salas <harley@harleysalas.com>
2020-03-21 01:42:08 +0300

    added news categories

--- src/components/PageSections/NewsPage/NewsCategories/NewsCategories.jsx
+++ src/components/PageSections/NewsPage/NewsCategories/NewsCategories.jsx
@@ -1,40 +1,32 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import './news-categories.scss';
- 
+import React from "react";
+import { Link } from "gatsby";
+import "./news-categories.scss";
+
+import UnderlineLink from "../../../UnderlineLink/UnderlineLink";
+
 const NewsCategories = () => {
-    return (
-        <div className="container news-categories">
-            <span className="news-categories__title">Browse By Category:</span>
-            <ul className="news-categories__ul">
-                <li className="news-categories__li">
-                    <Link to="/news/news">
-                        News
-                    </Link>
-                </li>
-                <li className="news-categories__li">
-                    <Link to="/news/press">
-                        Press
-                    </Link>
-                </li>
-                <li className="news-categories__li">
-                    <Link to="/news/awards">
-                        Awards
-                    </Link>
-                </li>
-                <li className="news-categories__li">
-                    <Link to="/news/event">
-                        Event
-                    </Link>
-                </li>
-                <li className="news-categories__li">
-                    <Link to="/news/story">
-                        Story
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
- 
+  return (
+    <div className="container news-categories">
+      <span className="news-categories__title">Browse By Category :</span>
+      <ul className="news-categories__ul">
+        <li className="news-categories__li">
+          <UnderlineLink linkTo="/news/news">News</UnderlineLink>
+        </li>
+        <li className="news-categories__li">
+          <UnderlineLink linkTo="/news/press">Press</UnderlineLink>
+        </li>
+        <li className="news-categories__li">
+          <UnderlineLink linkTo="/news/awards">Awards</UnderlineLink>
+        </li>
+        <li className="news-categories__li">
+          <UnderlineLink linkTo="/news/event">Event</UnderlineLink>
+        </li>
+        <li className="news-categories__li">
+          <UnderlineLink linkTo="/news/story">Story</UnderlineLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default NewsCategories;
