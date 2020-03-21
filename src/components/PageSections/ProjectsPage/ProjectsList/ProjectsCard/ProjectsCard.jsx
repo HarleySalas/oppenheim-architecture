a3e5eeee103728403b08df13c4fea39b8775dca7
Harley Salas <harley@harleysalas.com>
2020-03-21 15:29:51 +0300

    added link indicator

--- src/components/PageSections/ProjectsPage/ProjectsList/ProjectsCard/ProjectsCard.jsx
+++ src/components/PageSections/ProjectsPage/ProjectsList/ProjectsCard/ProjectsCard.jsx
@@ -4,6 +4,8 @@ import Img from "gatsby-image";
 import "./projects-card.scss";
 
 import Pin from "../../../../../shared/icons/pin-basic-stroke-01.svg";
+import CirclePath from "../../../../../shared/icons/circle-01.svg";
+import Arrow from "../../../../../shared/icons/arrow-stroke-01.svg";
 
 const ProjectsCard = ({ project }) => {
   //Try fixed Img instead of Fluid
@@ -11,10 +13,19 @@ const ProjectsCard = ({ project }) => {
   return (
     <Link to={project.node.fields.slug} className="projects-card__link">
       <div className="projects-card">
-        <Img
-          fluid={project.node.frontmatter.thumbnail.childImageSharp.fluid}
-          className="projects-card__img-wrapper"
-        />
+        <div className="projects-card__img-container">
+          <Img
+            fluid={project.node.frontmatter.thumbnail.childImageSharp.fluid}
+            className="projects-card__img-wrapper"
+          />
+          <div className="projects-card__img-overlay">
+            <button className="projects-card__btn">
+              <CirclePath className="projects-card__btn__circle projects-card__btn__circle--bottom" />
+              <CirclePath className="projects-card__btn__circle projects-card__btn__circle--top" />
+              <Arrow className="projects-card__btn__arrow" />
+            </button>
+          </div>
+        </div>
         <div className="projects-card__content-wrapper">
           <span className="projects-card__location">
             <Pin className="projects-card__pin" />
